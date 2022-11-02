d750e37b12f0f26c9ce622c90660b4b682c9cdf3
Max Vodolazhsky <maximazerr@mail.ru>
2022-11-02 20:25:30 +0300

    Switch int to long types

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[]{1, 2, 6});
+        SalesManager salesManager = new SalesManager(new long[]{1, 2, 6});
 
         System.out.println(salesManager.max());
     }
