755bf991cbdbe6ed05a74f8fc53e18e075456065
Max Vodolazhsky <maximazerr@mail.ru>
2022-11-02 20:17:38 +0300

    Add SalesManager and Readme.md

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        SalesManager salesManager = new SalesManager(new int[]{1, 2, 6});
+
+        System.out.println(salesManager.max());
+    }
+}
